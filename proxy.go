d8767e87335e19ad1996e04b7e6aee3871f21b61
Gianni Moschini <gianni.moschini@hailocab.com>
2014-06-01 10:06:23 +0100

    Save only text response and fix mgo.Collection

--- proxy.go
+++ proxy.go
@@ -49,7 +49,7 @@ func main() {
 
 	flag.Parse()
 
-	c := &mgo.Collection{}
+	c := new(mgo.Collection)
 
 	if len(*mongourl) != 0 {
 		// Mongo DB connection
@@ -80,12 +80,13 @@ func main() {
 			host = strings.Replace(host, ".mocky.dev", "", 1)
 			host = strings.Replace(host, "-", ".", -1)
 			host = strings.Replace(host, "..", "-", -1)
-			log.Printf("Target Host: %s", host)
-			req.Host = host
-
 		}
 
-		if c != nil && *mock && req.Method != "CONNECT" {
+		log.Printf("Target Host: %s", host)
+		req.Host = host
+		log.Printf("%+v", req)
+
+		if c.Database != nil && *mock && req.Method != "CONNECT" {
 			result := Content{}
 			ctx.Logf("Looking for existing request")
 			/*fmt.Println("RequestURI:", req.RequestURI)
@@ -121,26 +122,39 @@ func main() {
 	proxy.OnResponse().Do(goproxy.HandleBytes(
 		func(b []byte, ctx *goproxy.ProxyCtx) []byte {
 			ctx.Logf("Method: %s - host: %s", ctx.Resp.Request.Method, ctx.Resp.Request.Host)
-			if c != nil && ctx.UserData != nil && ctx.UserData.(ContextUserData).Store && ctx.Resp.StatusCode == 200 && ctx.Resp.Request.Method != "CONNECT" {
-				// Get Body as string
-				s := string(b[:])
+			if c.Database != nil && ctx.UserData != nil && ctx.UserData.(ContextUserData).Store && ctx.Resp.Request.Method != "CONNECT" {
 				contentType := ctx.Resp.Header.Get("Content-Type")
 				ctx.Logf(contentType)
 				arr := strings.Split(contentType, ";")
 				switch arr[0] {
 				case "application/json", "text/html":
-					ctx.Logf("Recording both request/response")
-					content := Content{
-						//Id: bson.NewObjectId(),
-						Request:  Request{Path: ctx.Resp.Request.URL.Path, Host: ctx.Resp.Request.Host, Method: ctx.Resp.Request.Method, Time: float32(time.Now().UnixNano()-ctx.UserData.(ContextUserData).Time) / 1.0e9, Headers: ctx.Resp.Request.Header},
-						Response: Response{Status: ctx.Resp.StatusCode, Headers: ctx.Resp.Header, Body: s},
-						Date:     time.Now(),
+					// Get Body as string
+					s := string(b[:])
+
+					if (ctx.Resp.StatusCode == 200) {
+						ctx.Logf("Recording both request/response")
+
+						content := Content{
+							//Id: bson.NewObjectId(),
+							Request: Request{
+								Path:    ctx.Resp.Request.URL.Path,
+								Host:    ctx.Resp.Request.Host,
+								Method:  ctx.Resp.Request.Method,
+								Time:    float32(time.Now().UnixNano()-ctx.UserData.(ContextUserData).Time) / 1.0e9,
+								Headers: ctx.Resp.Request.Header},
+							Response: Response{
+								Status:  ctx.Resp.StatusCode,
+								Headers: ctx.Resp.Header,
+								Body:    s},
+							Date: time.Now(),
+						}
+
+						err := c.Insert(content)
+						if err != nil {
+							ctx.Logf("Can't insert document: %v\n", err)
+						}
 					}
 
-					err := c.Insert(content)
-					if err != nil {
-						ctx.Logf("Can't insert document: %v\n", err)
-					}
 					ctx.Logf("Body: %s", s)
 				}
 			}
